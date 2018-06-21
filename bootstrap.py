1883d1f279d0cc76bd01cd32414228e9f4bfbb0e
Nail <nspahija15@epoka.edu.al>
2018-06-21 23:48:39 +0200

    Entrance Security

---
+++ bootstrap.py
@@ -0,0 +1,23 @@
+
+# handling information
+HOST = "localhost" #localhost
+PORT = 5000
+
+# make the url static
+url = 'http://127.0.0.1:8000/report/server'
+
+key = 'src/db/sercret_key.bin'
+images_path = "src/images/members/"
+tmpPath = "src/images/temp/"
+tmpFullImages_path = 'src/images/tmepFullImages/'
+not_members = 'src/images/notMembers/'
+db_path = 'src/db/members.db'
+logs_path = 'src/log/'
+save_temp_images_path = "src/images/"
+cascPath = "src/haarcascades/haarcascade_frontalface_alt2.xml"
+cascPath_full_body = "src/haarcascades/haarcascade_eye_tree_eyeglasses.xml"
+
+red = 255
+green = 255
+blue = 255
+border_size = 2
